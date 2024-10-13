867d02cc61901b0d8e1df938ff418c94f8a5e7d8
UGolen12 <emerdielle@gmail.com>
2024-10-13 18:40:59 +0700

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -9,5 +9,7 @@ def single_root_words(root_word, *other_words):
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+result3 = single_root_words('sun', 'sunny', 'sunflower', 'sunshine', 'son')
 print(result1)
 print(result2)
+print(result3)
